8ef51e6fb7c7a0f50ab28a20e70fd6a29b88b1e0
lekakid <neverguy123@naver.com>
2020-02-28 20:56:19 +0900

    Dotween 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject bush;
+    public Text tmp;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.anyKeyDown) {
+            tmp.enabled = false;
+            Time.timeScale = 1;
+        }
     }
 }
