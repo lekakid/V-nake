615dc3dc81b984f1f01346d665353965296fa7ee
lekakid <neverguy123@naver.com>
2020-03-20 14:27:58 +0900

    구조 캐릭터 카운트 추가

---
+++ Assets/Scripts/PlayView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayView : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI Count;
+
+    public void SetCount(int n) {
+        if(Count)
+            Count.SetText(string.Format("x{0:000}", n));
+    }
+}
