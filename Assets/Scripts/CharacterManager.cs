2830771b23933906794e906c45f3e40c26409ab9
lekakid <neverguy123@naver.com>
2020-03-22 19:50:48 +0900

    확률 및 접근자 조절

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -11,7 +11,7 @@ public class CharacterManager : MonoBehaviour
     public List<Rarity> Rarity;
     public List<Character> CharacterList;
     public Dictionary<Character, int> CharacterCount = new Dictionary<Character, int>();
-    public int TotalCount = 0;
+    int TotalCount = 0;
     
     public GameObject SpawnCharacter() {
         float r = Random.Range(0f, 100f);
