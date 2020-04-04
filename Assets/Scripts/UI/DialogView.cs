f2d38c873033d760046731bf3c5b73e5f98f34ee
LeKAKiD <lekakid@kakao.com>
2020-04-04 16:07:03 +0900

    다이얼로그 형태만 대충 잡음

---
+++ Assets/Scripts/UI/DialogView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogView : MonoBehaviour
+{
+    Image Background;
+    public GameObject Name;
+    public TextMeshProUGUI NameText;
+    public TextMeshProUGUI Content;
+
+    void Start()
+    {
+        Background = GetComponent<Image>();
+    }
+
+    void SetName(string name) {
+        NameText.SetText(name);
+    }
+
+    void PrintContent(string Text) {
+        
+    }
+}
