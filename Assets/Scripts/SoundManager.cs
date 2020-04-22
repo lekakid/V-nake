1c1f861c09837057de9420bb5113f30009c0ec3d
LeKAKiD <lekakid@kakao.com>
2020-04-22 18:44:04 +0900

    사운드 매니저 구조 잡아줌

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip[] BGM;
+    public AudioClip[] SFX;
+
+    AudioSource _bgmSource;
+    AudioSource _sfxSource;
+    Dictionary<string, AudioClip> _bgm;
+    Dictionary<string, AudioClip> _sfx;
+
+    void Awake()
+    {
+        _bgmSource = gameObject.AddComponent<AudioSource>();
+        _sfxSource = gameObject.AddComponent<AudioSource>();
+
+        _bgmSource.loop = true;
+
+        _bgm = new Dictionary<string, AudioClip>();
+        _sfx = new Dictionary<string, AudioClip>();
+
+        foreach(AudioClip a in BGM) {
+            _bgm.Add(a.name, a);
+        }
+
+        foreach(AudioClip a in SFX) {
+            _sfx.Add(a.name, a);
+        }
+    }
+
+    public void PlayBGM(string name) {
+        _bgmSource.clip = _bgm[name];
+        _bgmSource.Play();
+    }
+
+    public void PlaySFX(string name) {
+        _sfxSource.PlayOneShot(_sfx[name]);
+    }
+
+    public void SetBGMVolume(float value) {
+
+    }
+
+    public void SetSFXVolume(float value) {
+        
+    }
+}
