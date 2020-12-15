428af050bd01e3d99b480f81c463a45ec3532fd9
LeKAKiD <lekakid@kakao.com>
2020-12-16 00:52:18 +0900

    refactor: 컨트롤러 최후의 리팩토링

--- Assets/Scripts/View/CanvasView.cs
+++ Assets/Scripts/View/CanvasView.cs
@@ -2,27 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class CanvasView : MonoBehaviour
 {
+    CanvasGroup _canvasGroup;
     Animator _animator;
+    public bool hasShowAnimation;
+    public bool hasHideAnimation;
     public bool isAnimating { get; private set; }
 
     void Awake() {
+        _canvasGroup = GetComponent<CanvasGroup>();
         _animator = GetComponent<Animator>();
         
         RectTransform rect = GetComponent<RectTransform>();
         rect.offsetMin = Vector2.zero;
         rect.offsetMax = Vector2.zero;
 
-        gameObject.SetActive(false);
+        Hide();
     }
 
     public void Show() {
-        gameObject.SetActive(true);
+        _canvasGroup.alpha = 1;
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+
+        if(_animator && hasShowAnimation) {
+            _animator.SetTrigger("Show");
+        }
     }
 
     public void Hide() {
-        gameObject.SetActive(false);
+        _canvasGroup.alpha = 0;
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+
+        if(_animator && hasHideAnimation) {
+            _animator.SetTrigger("Hide");
+        }
     }
 
     public void OnAnimationEnter() {
