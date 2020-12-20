bd0e475c03db566e162a9aa6ca4743e8914fcd41
LeKAKiD <lekakid@kakao.com>
2020-12-20 09:34:43 +0900

    feat: 이스터에그 문 구현

---
+++ Assets/Scripts/Controller/ClubDoorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClubDoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public Transform Player;
+    public SpriteRenderer OpenedDoorRenderer;
+
+    void Awake() {
+        int brownieCount = Status.Instance.CharacterRescueCounts["Brownie"];
+        int goldenBrownieCount = Status.Instance.CharacterRescueCounts["GoldenBrownie"];
+
+        if(brownieCount >= 500 && goldenBrownieCount >= 1)
+            Door.SetActive(false);
+    }
+
+    void Update() {
+        OpenedDoorRenderer.sortingOrder = (Player.position.y > transform.position.y) ? 10 : 0;
+    }
+}
